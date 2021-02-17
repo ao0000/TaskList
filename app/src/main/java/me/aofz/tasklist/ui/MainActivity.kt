b8f02254d323d2aa71cc181b637803c8070f5a18
ao0000 <e1q19041@oit.ac.jp>
2021-02-17 20:52:14 +0900

    add bottom menu bar

--- app/src/main/java/me/aofz/tasklist/ui/MainActivity.kt
+++ app/src/main/java/me/aofz/tasklist/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package me.aofz.tasklist.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
+import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 import me.aofz.tasklist.R
 
 @AndroidEntryPoint
@@ -10,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.nav_Host_fragment)
+        ExpandableBottomBarNavigationUI.setupWithNavController(bottom_bar, navController)
     }
 }
