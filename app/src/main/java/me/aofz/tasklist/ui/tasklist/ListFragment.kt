54afa815cfd851734f0f029f60a4c442eb31f129
ao0000 <e1q19041@oit.ac.jp>
2021-04-20 19:00:17 +0900

    remove groupie

--- app/src/main/java/me/aofz/tasklist/ui/tasklist/ListFragment.kt
+++ app/src/main/java/me/aofz/tasklist/ui/tasklist/ListFragment.kt
@@ -9,12 +9,11 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.wada811.viewbinding.viewBinding
-import com.xwray.groupie.*
 import dagger.hilt.android.AndroidEntryPoint
 import me.aofz.tasklist.R
 import me.aofz.tasklist.databinding.ListFragmentBinding
 import me.aofz.tasklist.ext.hideKeyboard
-import me.aofz.tasklist.ui.tasklist.item.TaskItem
+import me.aofz.tasklist.model.Task
 
 @AndroidEntryPoint
 class ListFragment : Fragment(R.layout.list_fragment) {
@@ -22,64 +21,56 @@ class ListFragment : Fragment(R.layout.list_fragment) {
     private val listFragmentBinding by viewBinding(ListFragmentBinding::bind)
     private val listViewModel by viewModels<ListViewModel>()
 
-    private val groupAdapter = GroupAdapter<GroupieViewHolder>()
-    private val taskSection = Section()
+    private val adapter = TaskListAdapter { item: Task ->
+        val action = ListFragmentDirections.actionListFragmentToDetailFragment(item)
+        findNavController().navigate(action)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        listFragmentBinding.lifecycleOwner = viewLifecycleOwner
         listFragmentBinding.viewModel = listViewModel
         hideKeyboard()
 
-        groupAdapter.apply {
-            setOnItemClickListener(onItemClickListener)
-        }
-
-        listFragmentBinding.listRecyclerView.apply {
-            this.adapter = groupAdapter
-            ItemTouchHelper(touchCallback).attachToRecyclerView(this)
-        }
+        listFragmentBinding.listRecyclerView.adapter = adapter
+        initSwiped()
         observeList()
-    }
-
-    private val onItemClickListener = OnItemClickListener { item, _ ->
-        val taskItem = item as TaskItem
-        val action = ListFragmentDirections.actionListFragmentToDetailFragment(taskItem.task)
-        findNavController().navigate(action)
     }
 
     private fun observeList() {
         listViewModel.allTask.observe(viewLifecycleOwner, Observer {
             it?.let {
-                val listInSection = mutableListOf<Group>()
-                it.forEach {
-                    listInSection.add(TaskItem(it))
-                }
-                taskSection.update(listInSection)
-                groupAdapter.update(listOf(taskSection))
+                adapter.submitList(it)
             }
         })
     }
 
-    private val touchCallback: TouchCallback by lazy {
-        object : TouchCallback() {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
 
-            override fun isItemViewSwipeEnabled(): Boolean {
-                return true
-            }
+    private fun initSwiped() {
+        ItemTouchHelper(
+            object : ItemTouchHelper.Callback() {
+                override fun getMovementFlags(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder
+                ): Int = makeMovementFlags(
+                    0,
+                    ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+                )
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val item = groupAdapter.getItem(viewHolder.adapterPosition) as TaskItem
-                listViewModel.deleteTask(item.task)
-                taskSection.remove(item)
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean = false
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val taskViewHolder: TaskViewHolder = viewHolder as TaskViewHolder
+                    taskViewHolder.value?.let {
+                        listViewModel.deleteTask(it)
+                    }
+                }
             }
-        }
+        ).attachToRecyclerView(listFragmentBinding.listRecyclerView)
     }
 
 }
---
+++ app/src/main/java/me/aofz/tasklist/ui/tasklist/TaskListAdapter.kt
@@ -0,0 +1,45 @@
+package me.aofz.tasklist.ui.tasklist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import me.aofz.tasklist.databinding.ListItemBinding
+import me.aofz.tasklist.model.Task
+
+class TaskListAdapter(private val onClicked: (task: Task) -> Unit) :
+    ListAdapter<Task, TaskViewHolder>(DiffCallBack()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder =
+        TaskViewHolder.create(parent)
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int): Unit =
+        holder.bind(getItem(position), onClicked)
+}
+
+class TaskViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    var value: Task? = null
+
+    fun bind(task: Task, onClicked: (task: Task) -> Unit) {
+        value = task
+        binding.task = task
+        binding.root.setOnClickListener { onClicked(task) }
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): TaskViewHolder {
+            val binding: ListItemBinding =
+                ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return TaskViewHolder(binding)
+        }
+    }
+}
+
+class DiffCallBack : DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean =
+        oldItem == newItem
+}
