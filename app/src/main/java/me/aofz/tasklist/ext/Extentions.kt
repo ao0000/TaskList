50148976616f0d5c7696649da672deb572bda8c3
ao0000 <e1q19041@oit.ac.jp>
2021-04-23 01:29:49 +0900

    set gone descrption's View when it is no text

--- app/src/main/java/me/aofz/tasklist/ext/Extentions.kt
+++ app/src/main/java/me/aofz/tasklist/ext/Extentions.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 
 fun Fragment.hideKeyboard() {
@@ -16,4 +19,9 @@ fun Context.hideKeyboard(view: View) {
     val inputMethodManager: InputMethodManager =
         getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+@BindingAdapter("setVisibility")
+fun TextView.setVisibility(visibility: Boolean) {
+    this.isVisible = visibility
 }
