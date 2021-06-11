be3d6f5af39d93962e97223e0cdcfbb0d3d24005
ao0000 <e1q19041@oit.ac.jp>
2021-06-11 12:49:38 +0900

    keyboard hide bottom navigation

---
+++ app/src/test/java/me/aofz/tasklist/ui/taskdetail/DetailViewModelTest.kt
@@ -0,0 +1,34 @@
+package me.aofz.tasklist.ui.taskdetail
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DetailViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getTask() {
+    }
+
+    @Test
+    fun getDeleteButtonClicked() {
+    }
+
+    @Test
+    fun deleteTask() {
+    }
+
+    @Test
+    fun setUpTask() {
+    }
+}
